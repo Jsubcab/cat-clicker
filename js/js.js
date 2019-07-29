92fb75b0b41aa39a1c79b69717416a43bc2a8e5b
jsubcab <jsubcab@gmail.com>
2019-07-29 14:57:34 +0300

    updated and working

--- js/js.js
+++ js/js.js
@@ -1,20 +1,28 @@
 const redCat = document.getElementById("redCat");
 const blackCat = document.getElementById("blackCat");
-const counterBlack = document.getElementById("counter-black");
-const counterRed = document.getElementById("counter-red");
+const counterBlack = document.querySelector(".counter-black-cat");
+const counterRed = document.querySelector(".counter-red-cat");
+
 let redNumCounter = 0;
 let blackNumCounter = 0;
-const nameRed = "Rojo";
-const nameBlack = "Negro";
+let node = document.createElement("p");
+let node2 = document.createElement("p");
 
-counterRed.innerHTML;
+let rojo = document.createTextNode('Kira');
+node2.appendChild(rojo);
+counterRed.appendChild(node2);
+
 redCat.addEventListener('click', function(){
     redNumCounter++;
-    counterRed.innerHTML = "Counter: " + redNumCounter;
+    document.getElementById('counter-red').innerHTML = "Counter: " + redNumCounter;
 });
 
-counterRed.appendChild(nameBlack);
+
+let negro = document.createTextNode('Pituca');
+node.appendChild(negro);
+counterBlack.appendChild(node);
+
 blackCat.addEventListener('click', function(){
     blackNumCounter++;
-    counterBlack.innerHTML = "Counter: " + blackNumCounter;
+    document.getElementById('counter-black').innerHTML = "Counter: " + blackNumCounter;
 });
